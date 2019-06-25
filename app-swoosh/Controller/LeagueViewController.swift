fe44f366e1667b05bfe843c3e3e50824543d2408
Anthony <cozziad@gmail.com>
2019-06-25 12:04:09 -0400

    Lecture 046 Done

--- app-swoosh/Controller/LeagueViewController.swift
+++ app-swoosh/Controller/LeagueViewController.swift
@@ -10,14 +10,37 @@ import UIKit
 
 class LeagueViewController: UIViewController {
 
+    var player: Player!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        nextBtn.isEnabled = false
+        player = Player()
     }
     
+   
+    @IBAction func mensTapped(_ sender: Any) {
+        selectedLeague(leagueType: PlayerType.mens)
+    }
+    
+    @IBAction func womensTapped(_ sender: Any) {
+        selectedLeague(leagueType: PlayerType.womens)
+    }
+    
+    @IBAction func coedTapped(_ sender: Any) {
+        selectedLeague(leagueType: PlayerType.coed)
+    }
+    
+    @IBOutlet weak var nextBtn: BorderButton!
+    
     @IBAction func onNextTapped(_ sender: Any) {
+        
         performSegue(withIdentifier: "skillVCSeque", sender: self)
+    }
+    
+    func selectedLeague(leagueType: PlayerType){
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
     }
 
 }
---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,27 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Anthony Cozzi on 6/25/19.
+//  Copyright © 2019 Anthony Cozzi. All rights reserved.
+//
+
+import Foundation
+
+struct Player{
+    var desiredLeague: PlayerType?
+    var selectedSkillLevel: PlayerSkillLevel?
+}
+
+enum PlayerType
+{
+    case womens
+    case mens
+    case coed
+}
+
+enum PlayerSkillLevel
+{
+    case beginner
+    case baller
+}
